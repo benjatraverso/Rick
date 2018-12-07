4f7da522c377c1e188950d2c94f169b147686603
Benjamín Traverso <benjatraverso@users.noreply.github.com>
2018-12-07 19:21:13 -0300

    Finshed
    End of implementation with random time added to next trigger.

--- Prank.ino
+++ Prank.ino
@@ -26,16 +26,51 @@
 #define  a5f    831     // 831 Hz 
 
 int giWDCounter = 0;
+const int buzzer = 2;
+
+volatile int beatlength = 120; // determines tempo
+int beatseparationconstant = 0.3;
+//repetitions:
+//intervals are a random in between min and max
+int giRepetitions = 60/8 * 60 * 24;		//first call is triggered
+const int minTime = 60/8 * 60;			//least time interval
+const int maxTime = 60/8 * 60 * 2;	//top time interval
+
+int song1_chorus_melody[] =
+{ b4f, b4f, a4f, a4f,
+  f5, f5, e5f, b4f, b4f, a4f, a4f, e5f, e5f, c5s, c5, b4f,
+  c5s, c5s, c5s, c5s,
+  c5s, e5f, c5, b4f, a4f, a4f, a4f, e5f, c5s,
+  /*
+  b4f, b4f, a4f, a4f,
+  f5, f5, e5f, b4f, b4f, a4f, a4f, a5f, c5, c5s, c5, b4f,
+  c5s, c5s, c5s, c5s,
+  c5s, e5f, c5, b4f, a4f, rest, a4f, e5f, c5s, rest
+  */
+};
+
+int song1_chorus_rhythmn[] =
+{ 1, 1, 1, 1,
+  3, 3, 6, 1, 1, 1, 1, 3, 3, 3, 1, 2,
+  1, 1, 1, 1,
+  3, 3, 3, 1, 2, 2, 2, 4, 8,
+  /*
+  1, 1, 1, 1,
+  3, 3, 6, 1, 1, 1, 1, 3, 3, 3, 1, 2,
+  1, 1, 1, 1,
+  3, 3, 3, 1, 2, 2, 2, 4, 8, 4
+  */
+};
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  DDRB = 0b0010;  
+  pinMode(buzzer, OUTPUT);
   //let us know the device is on
-  PORTB = 0b0010;
-  delay(100);
-  PORTB = 0b0000;
+  digitalWrite(buzzer,HIGH);
+  delay(1000);
+  digitalWrite(buzzer,LOW);
   
-  cbi(ADCSRA,ADEN);//disable ADC, no need on this project
+  cbi(ADCSRA,ADEN);		// disable ADC, no need on this project
   cbi( MCUCR,SE );      // sleep enable, power down mode
   cbi( MCUCR,SM0 );     // power down mode
   sbi( MCUCR,SM1 );     // power down mode
@@ -44,39 +79,33 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  //WDIE is cleared after each interrupt so this bit should be re-set
+  WDTCR |= _BV(WDIE);
   system_sleep();
-  if(giWDCounter == 400)
+  if(giWDCounter >= giRepetitions)
   {
-    giWDCounter = 0;
     annoy();
+    //change time for next call
+    giRepetitions = random(minTime,maxTime);
+    giWDCounter = 0;
   }
 }
 
 void annoy()
 {
   int b = 0;  
-    int song1_chorus_melody[] =
-  { b4f, b4f, a4f, a4f,
-    f5, f5, e5f, b4f, b4f, a4f, a4f, e5f, e5f, c5s, c5, b4f,
-    c5s, c5s, c5s, c5s,
-    c5s, e5f, c5, b4f, a4f, a4f, a4f, e5f, c5s,
-  };
-  int song1_chorus_rhythmn[] =
-  { 1, 1, 1, 1,
-    3, 3, 6, 1, 1, 1, 1, 3, 3, 3, 1, 2,
-    1, 1, 1, 1,
-    3, 3, 3, 1, 2, 2, 2, 4, 8,
-  };
+  int chorus_length = sizeof(song1_chorus_melody) / sizeof(int);
   
-  while(b < sizeof(song1_chorus_melody) / sizeof(int))
+  while(b < chorus_length)
   {
+    int notelength = beatlength * song1_chorus_rhythmn[b];
     if (song1_chorus_melody[b] > 0) {
-      tone(2, song1_chorus_melody[b], 120 * song1_chorus_rhythmn[b]);
+      tone(buzzer, song1_chorus_melody[b], notelength);
     }
     b++;
-    delay(120 * song1_chorus_rhythmn[b]);
-    noTone(2);
-    delay(120 * song1_chorus_rhythmn[b] * 0.3);
+    delay(notelength);
+    noTone(buzzer);
+    delay(notelength * beatseparationconstant);
   }
 }
 
@@ -101,7 +130,7 @@ void setup_watchdog(int ii) {
  WDTCR |= (1<<WDCE) | (1<<WDE);
  // set new watchdog timeout value
  WDTCR = bb;
- WDTCR |= _BV(WDTIE);
+ WDTCR |= _BV(WDIE);
 }
 
 //****************************************************************  
@@ -113,7 +142,7 @@ void system_sleep() {
  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
  sleep_enable();
  sleep_mode();                        // System sleeps here
- sleep_disable();              // System continues execution here when watchdog timed out 
+ sleep_disable();              // System continues execution here when watchdog timed out
  //we dont need ADC on this projetc
  //sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
 }
